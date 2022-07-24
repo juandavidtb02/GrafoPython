bbda8c9c8072b3389349de5a946a983db6ed1c70
juan david <jdtorres@unillanos.edu.co>
2022-07-24 17:17:07 -0500

    se añade comentario al programa

--- algoritmodijkstra.py
+++ algoritmodijkstra.py
@@ -3,6 +3,7 @@
 #v = ['1','2','3','4','5']
 #a = [[0,100,30,0,0],[0,0,20,0,0],[0,0,0,10,60],[0,15,0,0,50],[0,0,0,0,0]]
 
+#clase dijkstra
 class dis:
     def __init__(self,v,matriz,origen,destino):
         self.v = v
@@ -20,42 +21,55 @@ class dis:
                     self.matriz.append([self.v[ac], self.v[fc], self.a[ac][fc]])
 
     def getCosto(self):
+        #funcion para determinar el costo del algoritmo dijkstra
         for i in range(len(self.dijkstra)):
             self.costo = self.dijkstra[i][2] + self.costo
         return self.costo
 
     def algoritmo_dijkstra(self):
-
+        #funcion algoritmo dijksra
         first_iteracion = True
         peso = 0
         destinopaso = ''
         visitados = []
         existe = False
-
+        #se verifica si el vertice origen y el vertice destino son diferentes
         if self.origen != self.destino:
             llegada = False
 
+        #ciclo para encontrar la ruta, no para hasta encontrar la ruta
         while not llegada:
+            #for para recorrer toda la matriz de aristas
             for i in range(len(self.matriz)):
+                #verifica si la arista no apunta al mismo vertice
                 if self.matriz[i][0] != self.matriz[i][1]:
+                    #verifica si el origen actual coincide con las aristas que se estan iterando en el for
                     if self.origen == self.matriz[i][0]:
+                        #para comparar los pesos, en cada inicio del for se crea la variable peso para comparar despues
                         if first_iteracion:
                             peso = self.matriz[i][2] + 1
                             first_iteracion = False
+                        #se busca el peso menor y si la arista no ha sido registrada anteriormente (para evitar ciclos)
                         if peso > self.matriz[i][2] and self.matriz[i] not in self.dijkstra and not existe:
                             peso = self.matriz[i][2]
                             destinopaso = self.matriz[i][1]
                         else:
+                            #se utiliza el segundo menor en caso de que exista un ciclo
                             if existe:
                                 peso = self.matriz[i][2]
                                 destinopaso = self.matriz[i][1]
                             existe = True
 
+            #se reinician los booleanos necesarios
             first_iteracion = True
             existe = False
+            #se guarda la arista en el camino dijkstra
             self.dijkstra.append([self.origen, destinopaso, peso])
+            #se guarda el vertice origen para indicar que ya se visito
             visitados.append(self.origen)
+            #se establece que el nuevo origen es la variable destino de la arista guardada
             self.origen = destinopaso
+            #si ya la variable destinopaso es igual al destino original, se acaba el while
             if destinopaso == self.destino:
                 llegada = True
         return self.dijkstra
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
 
+#algoritmo dijkstra
 from algoritmodijkstra import dis
 
 #creacion de la ventana
@@ -29,10 +30,10 @@ title2.pack()
 #se declara un vector para guardar los vertices
 frameContent = Frame(ventana,width=400,height=370,bg=bg_fondo)
 frameContent.pack(pady=60)
-#titulo primera entrada
+#texto entradas
 request = tk.Label(frameContent,text="Inserte numero de vertices",font=('Ubuntu',12),bg=bg_fondo,fg="white")
 request.pack(pady=5)
-
+#texto fallo
 fail = tk.Label(frameContent,text="Ingrese un numero valido de vertices",font=('Arial',12),bg=bg_fondo,fg="white")
 
 
@@ -41,6 +42,7 @@ gVertices = tk.Entry(frameContent, width=5, font=('Ubuntu',12),bg='#BFBFBF' )
 gVertices.insert(0,'1')
 gVertices.pack(ipadx=5,ipady=5,pady=2)
 
+#entrada origen y destino dijkstra
 frameOD = Frame(frameContent, width=200, height=200,bg=bg_fondo)
 origen = Entry(frameOD, width=5, font=('Ubuntu',12),bg='#BFBFBF' )
 destino = Entry(frameOD, width=5, font=('Ubuntu', 12), bg='#BFBFBF')
@@ -55,23 +57,31 @@ matrizF = []
 #guarda los valores de la matriz de adyacencia
 matrizGrafica = []
 valorEtiquetas = []
+#numero de vertices
 numv = 0
-matrizxd = []
+#matriz que contiene las aristas
+matrizAristas = []
+#objeto que guarda el grafo
 Grafo = nx.DiGraph()
 def dijkstra():
     frameBotones.pack_forget()
     frameOD.pack_forget()
     request.pack_forget()
     title2['text'] = 'Algoritmo Dijkstra'
-    d = dis(valorEtiquetas,matrizxd,origen.get(),destino.get())
+    #creacion objeto dijkstra para realizar el algoritmo
+    #el constructor de la clase requiere: las etiquetas, las aristas, el origen y el destino
+    d = dis(valorEtiquetas,matrizAristas,origen.get(),destino.get())
+    #se realiza el algoritmo dijkstra y se almacena el resultado
     resultado = d.algoritmo_dijkstra()
     frameResultado.pack()
 
+    #creacion de las matrices necesaria para mostrar en pantalla el resultado
     matrizPasos = []
     matrizResultados = []
     for i in range(len(resultado)):
         matrizResultados.append([0] * 3)
 
+    #Crecion de la matriz grafica para mostrar el dijkstra
     pasoTitulo = Label(frameResultado, text='Paso', font=('Arial', 12), pady=3, bg=bg_fondo, fg="white")
     origenTitulo = Label(frameResultado,text='Origen', font=('Arial', 12), pady=3,bg=bg_fondo,fg="white")
     destinoTitulo = Label(frameResultado, text='Destino', font=('Arial', 12), pady=3, bg=bg_fondo, fg="white")
@@ -102,6 +112,7 @@ def dijkstra_request():
     frameBotones.pack_forget()
     frameContent.pack(pady=60)
     frameOD.pack(pady=5)
+    #entrada origen y destino dijkstra
     request['text'] = 'Inserte origen y destino:'
     labelOrigen = Label(frameOD,text='Inserte origen: ', font=('Arial', 12), pady=3,bg=bg_fondo,fg="white")
     labelDestino = Label(frameOD,text='Inserte destino: ', font=('Arial', 12), pady=3,bg=bg_fondo,fg="white")
@@ -117,7 +128,9 @@ def dijkstra_request():
 
 
 def grafo():
+    global matrizAristas
     validarEtiquetas = True
+    #verificacion si matriz de adyacencia es valida
     for a in range(numv):
         for fc in range(numv):
             value = matrizGrafica[fc][a].get()
@@ -126,16 +139,18 @@ def grafo():
 
     if validarEtiquetas:
         fail.pack_forget()
+        #almacenamiento de matriz de adyacencia
         for a in range(numv):
             for fc in range(numv):
                 if int(matrizGrafica[fc][a].get()) != 0:
-                    matrizxd.append([valorEtiquetas[a],valorEtiquetas[fc],int(matrizGrafica[fc][a].get())])
-                    print(valorEtiquetas[a] + ' - ' + valorEtiquetas[fc] + ' - ' + matrizGrafica[fc][a].get())
+                    matrizAristas.append([valorEtiquetas[a],valorEtiquetas[fc],int(matrizGrafica[fc][a].get())])
+                    #print(valorEtiquetas[a] + ' - ' + valorEtiquetas[fc] + ' - ' + matrizGrafica[fc][a].get())
+                    #se añaden los vertices segun la matriz de adyacencia introducida
                     Grafo.add_edge(valorEtiquetas[a], valorEtiquetas[fc], weight=int(matrizGrafica[fc][a].get()))
 
-        print(matrizxd)
+        #print(matrizAristas)
         print(Grafo)
-
+        #creacion de la imagen del grafo
         elarge = [(u, v) for (u, v, d) in Grafo.edges(data=True) if d["weight"] > 0.1]
         esmall = [(u, v) for (u, v, d) in Grafo.edges(data=True) if d["weight"] <= 0.5]
         pos = nx.spring_layout(Grafo, seed=1)  # positions for all nodes - seed for reproducibility
@@ -184,7 +199,7 @@ def grafo():
         fail.pack(pady=1)
 
 def matriz():
-
+    #verificacion si etiquetas son validas
     validarEtiquetas = True
     for i in range(0,numv):
         value = gEtiquetas[i].get()
@@ -193,8 +208,10 @@ def matriz():
             break
 
     if validarEtiquetas:
+        #matriz de adyacencia
         fail.pack_forget()
         request['text'] = "Ingrese la matriz de adyacencia"
+        #se añaden las etiquetas al grafo para asi tener los vertices
         for i in range(0, numv):
             valorEtiquetas.append(gEtiquetas[i].get())
             Grafo.add_nodes_from(valorEtiquetas)
@@ -205,7 +222,7 @@ def matriz():
         frameContent.pack_forget()
         frameContent.pack(pady=10)
         frameMatriz.pack()
-
+        #construccion de matriz de adyacencia grafica
         for i in range(numv):
             matrizGrafica.append([0] * numv)
 
@@ -231,22 +248,24 @@ def matriz():
 def numVertices():
     global vertices
     global numv
+    #se almacena el numero de vertices introducido
     content = gVertices.get()
-
+    #verificacion si es una entrada valida
     if content.isdigit() and content != '0':
         fail.pack_forget()
         numv = int(content)
         print(numv)
-
+        #nuevo texto
         gVertices.destroy()
         boton1.pack_forget()
         request['text'] = "Ingrese las etiquetas"
 
         global gEtiquetas
         # entryEtiquetas = []
+        # vector para almacenar las etiquetas
         etiqueta = []
-
         frameEtiquetas.pack(pady=5)
+        #entradas etiquetas
         for i in range(0, numv):
             etiqueta.append(Label(frameEtiquetas, text=f"Vertice {i + 1}: ", font=('Arial', 12), pady=3,bg=bg_fondo,fg="white"))
             etiqueta[i].grid(row=i, column=0)
